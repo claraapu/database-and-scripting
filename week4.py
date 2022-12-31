a8d73b378a28162d14a390c1d734d919af1f5b9a
claraapu <113291214+claraapu@users.noreply.github.com>
2022-12-31 15:59:39 -0700

    Add files via upload

---
+++ week4.py
@@ -0,0 +1,23 @@
+# Example of a for loop
+
+numbers = [1,2,3,4,5,6]
+
+for i in numbers:
+    print(i)
+
+# Example of a while loop
+
+i = 10
+
+while i > 5:
+    i -= 1
+    print(i)
+
+
+# Using a for loop and if statemnt, print the following numbers: 15, 55, 75, 150
+
+numbers = [15, 55, 75, 150]
+
+for i in numbers:
+    if 1 == 1:
+        print(i)
